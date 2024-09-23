c1144aafc0cdd2dc5d1fd4b0a951d8f9e27105d9
lunoxdev <lunox.code@gmail.com>
2024-09-23 12:29:15 -0500

    update url info

--- src/services/supabase.jsx
+++ src/services/supabase.jsx
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "SUPABASE_URL HERE";
-const supabaseAnonKey = "SUPABASE_ANON_KEY HERE";
+const supabaseUrl = "SUPABASE_URL IN .ENV FILE";
+const supabaseAnonKey = "SUPABASE_ANON_KEY IN .ENV FILE";
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
